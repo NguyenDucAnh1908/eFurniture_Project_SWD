296fe8a0b2e1132e84556dc06f22f7a9c70962b6
NguyenDucAnh1908 <133002625+NguyenDucAnh1908@users.noreply.github.com>
2024-01-21 16:44:15 +0700

    First commit
    Create project

---
+++ src/main/java/com/eFurnitureproject/eFurniture/EFurnitureApplication.java
@@ -0,0 +1,13 @@
+package com.eFurnitureproject.eFurniture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EFurnitureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EFurnitureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eFurnitureproject/eFurniture/EFurnitureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eFurnitureproject.eFurniture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EFurnitureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
