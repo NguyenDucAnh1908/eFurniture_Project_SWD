3933f97667b9368d200371c4e8a764196557e86c
chanhtong123 <chanhtong11@gmail.com>
2024-02-01 14:18:43 +0700

    Response(Blog and Address)

---
+++ src/main/java/com/eFurnitureproject/eFurniture/Responses/BlogResponse.java
@@ -0,0 +1,25 @@
+package com.eFurnitureproject.eFurniture.Responses;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class BlogResponse {
+    private Long id;
+    private String title;
+    private String content;
+    private String thumbnail;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime dateCreate;
+    private Long userBlogId; // Assuming you want to use userBlogId instead of User object directly
+
+    private Long tagBlogId; // Assuming you want to use tagBlogId instead of TagsBlog object directly
+
+    private Long categoryBlogId; // Assuming you want to use categoryBlogId instead of CategoryBlog object directly
+}
