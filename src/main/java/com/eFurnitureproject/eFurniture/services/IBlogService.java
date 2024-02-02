76510486331b3391052003a26f904289564b8722
chanhtong123 <chanhtong11@gmail.com>
2024-02-02 09:02:27 +0700

    BlogController

--- src/main/java/com/eFurnitureproject/eFurniture/services/IBlogService.java
+++ src/main/java/com/eFurnitureproject/eFurniture/services/IBlogService.java
@@ -1,10 +1,15 @@
 package com.eFurnitureproject.eFurniture.services;
 
 import com.eFurnitureproject.eFurniture.Responses.BlogResponse;
+import com.eFurnitureproject.eFurniture.dtos.BlogDto;
+import com.eFurnitureproject.eFurniture.models.Blog;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface IBlogService {
      Page<BlogResponse> getAllBlogs(String keyword, Pageable pageable,
                                           Long userBlogId, Long tagsBlogId);
+
+     Blog createBlog(BlogDto blogDto) throws EntityNotFoundException;
 }
